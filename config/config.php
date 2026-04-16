e285f5670b8100eb0d3fe8a34b347110a4a9a7c6
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2026-04-15 23:55:49 -0600

    first commit

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+    try {
+        define("HOST", "localhost");
+        define("DBNAME", "anime");
+        define("USER", "root");
+        define("PASS", "");
+
+        $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
