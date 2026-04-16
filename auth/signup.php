5cdb900ab483c40123ae5008192dbc6d789e8642
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2026-04-16 16:13:30 -0600

    Starting sessions and validating

--- auth/signup.php
+++ auth/signup.php
@@ -1,6 +1,10 @@
-<?php require "../includes/header.php"; ?>
-<?php require "../config/config.php"; ?>
+<?php require "../includes/header.php";?>
+<?php require "../config/config.php";?>
 <?php
+    if (isset($_SESSION['username'])) {
+        header("location: ".APPURL."");
+    }
+
     if (isset($_POST['submit'])) {
         if (empty($_POST['email']) OR empty($_POST['username']) OR empty($_POST['password'])) {
             echo "<script>alert('one or more inputs are empty')</script>";
--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,7 @@
 <?php
-    define("APPURL", "http://localhost/anime-main")
+    ob_start();
+    session_start();
+    define("APPURL", "http://localhost/anime-main");
 ?>
 
 <!DOCTYPE html>
@@ -41,7 +43,7 @@
             <div class="row">
                 <div class="col-lg-2">
                     <div class="header__logo">
-                        <a href="./index.html">
+                        <a href="<?php echo APPURL; ?>">
                             <img src="img/logo.png" alt="">
                         </a>
                     </div>
@@ -50,19 +52,27 @@
                     <div class="header__nav">
                         <nav class="header__menu mobile-menu">
                             <ul>
-                                <li><a href="./index.html">Homepage</a></li>
+                                <li><a href="<?php echo APPURL; ?>/index.php">Homepage</a></li>
                                 <li><a href="./categories.html">Categories <span class="arrow_carrot-down"></span></a>
                                     <ul class="dropdown">
-                                        <li><a href="./categories.html">Categories</a></li>
-                                        <li><a href="./anime-details.html">Anime Details</a></li>
-                                        <li><a href="./anime-watching.html">Anime Watching</a></li>
-                                        <li><a href="./blog-details.html">Blog Details</a></li>
-                                        <li><a href="./signup.html">Sign Up</a></li>
-                                        <li><a href="./login.html">Login</a></li>
+                                        <li><a href="./categories.html">Magic</a></li>
+                                        <li><a href="./categories.html">Adventure</a></li>
+                                        <li><a href="./categories.html">Action</a></li>
                                     </ul>
                                 </li>
-                                <li><a href="./blog.html">Our Blog</a></li>
-                                <li><a href="#">Contacts</a></li>
+                                <?php if (isset($_SESSION['username'])): ?>
+                                    <li>
+                                        <a href="#">
+                                            <?php echo $_SESSION['username'];?><span class="arrow_carrot-down"></span>
+                                        </a>
+                                        <ul class="dropdown">
+                                            <li><a href="./categories.html">Magic</a></li>
+                                            <li><a href="./categories.html">Adventure</a></li>
+                                            <li><a href="./categories.html">Action</a></li>
+                                            <li><a href="<?php echo APPURL; ?>/auth/logout.php">Logout</a></li>
+                                        </ul>
+                                    </li>
+                                <?php endif; ?>
                             </ul>
                         </nav>
                     </div>
@@ -70,7 +80,9 @@
                 <div class="col-lg-2">
                     <div class="header__right">
                         <a href="#" class="search-switch"><span class="icon_search"></span></a>
-                        <a href="./login.html"><span class="icon_profile"></span></a>
+                        <?php if (!isset($_SESSION['username'])) : ?>
+                            <a href="<?php echo APPURL; ?>./auth/login.php"><span class="icon_profile"></span></a>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
